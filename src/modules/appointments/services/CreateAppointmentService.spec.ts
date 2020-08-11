6535f2467f378b1154d2c14ba86e3ba0c8a8dfc6
Nathan Souza <nathancsouza@gmail.com>
2020-08-10 22:01:41 -0700

    first single test

---
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,21 @@
+import 'reflect-metadata';
+import CreateAppointmentService from './CreateAppointmentService';
+import FakeAppointmentRepository from '../repositories/fakes/FakeAppointmentsRepository';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentRepository();
+
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123123',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123123');
+  });
+});
