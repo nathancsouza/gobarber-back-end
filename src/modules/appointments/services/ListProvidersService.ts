4421a2d113757b74b64ccf921db13222abcb9e1c
Nathan Souza <nathancsouza@gmail.com>
2020-08-27 22:09:26 -0700

    removing unused variable

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -25,8 +25,6 @@ class ListProvidersService {
       `providers-list:${user_id}`
     );
 
-    // let users;
-
     if (!users) {
       users = await this.usersRepository.findAllProviders({
         except_user_id: user_id,
